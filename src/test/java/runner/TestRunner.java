ee0ffcea2f46a3c5e3a88226194b728120c2d29a
Rohit <urbanrohita07@gmail.com>
2024-07-26 17:55:01 +0530

    pom.xml changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,18 +1,18 @@
 package runner;
 
-import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
 @CucumberOptions(features="src/test/resources/features",
 		         glue= {"stepdefinitions","hooks"},
 		         publish= true,
 		         plugin = {"pretty","html:target/CucumberReports/CucumberReports.html"},
 		         dryRun = false
 		)
-public class TestRunner {	
+public class TestRunner extends AbstractTestNGCucumberTests{	
 	
 
 
